f68d1c96bf4a4d49228c470798658685ab5c241a
huiGavin <140878844+huiGavin@users.noreply.github.com>
2023-10-05 21:42:16 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,69 +1,38 @@
-window.localStorage.setItem('ceshi1', '1111111');
+var submitBtn = document.getElementById("submitBtn");
+  submitBtn.addEventListener("click", function(){
+      var username = document.getElementById("username").value;
+      var password = document.getElementById("password").value;
 
+      if(username === ""){
+         alert("请输入账号！");
+         return;
+      }
 
+      if(password === ""){
+        alert("请输入密码！");
+        return;
+      }
 
-function checkPassword(password) {
-    // 判断密码是否为空
-    if(!password) {
-        return " × 密码框为空";
+      if(username.length < 4 || username.length > 20){
+        alert("账号长度应为4-20个字符！");
+        return;
+      }
+
+      if(verify(username,password)){
+        //alert("登陆成功！");
+        window.location.href="leetcode.html";
+      }else{
+        alert("账号或密码错误，请重新输入！");
+      }
+
+  });
+
+  function verify(username, password){
+    if(username === "name" && password === "123456"){
+      return true;
+    }else{
+      return false;
     }
-
-    var hasUpper = /[A-Z]/.test(password); // 使用正则表达式判断是否包含大写字母
-    var hasLower = /[a-z]/.test(password); // 使用正则表达式判断是否包含小写字母
-
-    if(!hasUpper) {
-        return " × 至少一个大写字母";
-    }
-    if(!hasLower) {
-        return " × 至少一个小写字母";
-    }
-    
-    var re = /[^A-Za-z0-9]/; // 判断密码是否包含特殊字符
-    if(!re.test(password)) {
-        return " × 至少包含一个特殊字符";
-    }
-
-    // 判断密码长度
-    if(password.length < 6 || password.length > 16) {
-        return " × 长度在6-16个字符";
-    }
-
-    // 如果符合要求，则返回 true
-    return "密码正确";
-}
-
-
-function handleInput() {
- 
-    var password = document.getElementById("password").value; // 获取用户输入的密码
-    var check = checkPassword(password); //获取密码验证信息
-
-    if(check == "密码正确") {
-
-        var mySpan = document.getElementById("giao"); //获取<span> 标签的 DOM 节点
-        mySpan.style.color = "green"; //修改字体颜色
-        mySpan.innerText = " √ 密码格式正确"; // 或者使用 innerText 属性来修改内容
-
-    } else {
-
-        var mySpan = document.getElementById("giao"); //获取<span> 标签的 DOM 节点
-        mySpan.style.color = "red"; //修改字体颜色
-        mySpan.innerText = check; // 或者使用 innerText 属性来修改内容
-
-    }
-}
-
-
-document.getElementById("togglePassword").addEventListener("click", function() {
-    var passwordInput = document.getElementById("password");
-    if(passwordInput.type === "password") {
-        passwordInput.type = "text";
-    } else {
-        passwordInput.type = "password";
-    }
-});
-
-
-/**************************************/
+  }
 
        
